297c70cf43d2cb4e2ad3c81b960303be3f6c04ab
kmaasrud <km@aasrud.com>
2023-02-08 22:43:07 +0100

    add input and output args to CLI
    This commit also adds a help text, accessible with the `--help` flag, as
    well as a version text, available by using `--version`. My hope is that
    this commit will make the jotdown CLI a bit friendlier to use.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,84 @@
-use std::io::Read;
+use jotdown::{html, Parser};
+use std::{
+    ffi::OsString,
+    fs::{self, File},
+    io::{self, BufWriter},
+    process::exit,
+};
+
+#[derive(Default)]
+struct App {
+    input: Option<OsString>,
+    output: Option<OsString>,
+}
+
+fn parse_args() -> App {
+    let mut app = App::default();
+
+    let mut args = std::env::args_os().skip(1).peekable();
+
+    while let Some(arg) = args.next() {
+        match (arg.to_string_lossy().as_ref(), args.peek()) {
+            ("-h" | "--help", _) => {
+                eprint!("{}", include_str!("./help.txt"));
+                exit(0);
+            }
+            ("-v" | "--version", _) => {
+                eprintln!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+                exit(0);
+            }
+            (flag @ "-o" | flag @ "--output", o) => match o {
+                Some(o) => {
+                    app.output = Some(o.into());
+                    args.next();
+                }
+                None => {
+                    eprintln!("please supply an argument to {flag}");
+                    exit(1);
+                }
+            },
+            ("-", _) => {}
+            (file, _) if !file.starts_with('-') => {
+                if app.input.is_some() {
+                    eprint!("too many arguments\n\n{}", include_str!("./help.txt"));
+                    exit(1)
+                }
+                app.input = Some(file.into());
+            }
+            (flag, _) => {
+                eprint!("unknown flag: {flag}\n\n{}", include_str!("./help.txt"));
+                exit(1)
+            }
+        }
+    }
+
+    app
+}
+
+fn run() -> Result<(), io::Error> {
+    let app = parse_args();
+
+    let content = match app.input {
+        Some(path) => fs::read_to_string(path)?,
+        None => io::read_to_string(io::stdin())?,
+    };
+
+    let parser = Parser::new(&content);
+
+    match app.output {
+        Some(path) => html::write(parser, File::create(path)?)?,
+        None => html::write(parser, BufWriter::new(io::stdout()))?,
+    }
+
+    Ok(())
+}
 
 fn main() {
-    let mut src = String::new();
-    std::io::stdin()
-        .read_to_string(&mut src)
-        .expect("failed to read utf-8 file");
-
-    let events = jotdown::Parser::new(&src);
-    let mut out = std::io::BufWriter::new(std::io::stdout());
-    jotdown::html::write(events, &mut out).unwrap();
+    match run() {
+        Ok(()) => {}
+        Err(e) => {
+            eprintln!("{e}");
+            exit(1);
+        }
+    }
 }
