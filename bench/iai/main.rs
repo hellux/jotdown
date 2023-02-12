a36cb74c6635771d18b2ee28cd12551f411221ba
Noah Hellman <noah@hllmn.net>
2023-02-12 13:31:14 +0100

    add iai benchmarks

---
+++ bench/iai/main.rs
@@ -0,0 +1,21 @@
+use iai::black_box;
+
+use jotdown::Render;
+
+fn block() -> jotdown::Parser<'static> {
+    jotdown::Parser::new(black_box(bench_input::ALL))
+}
+
+fn block_inline() -> Option<jotdown::Event<'static>> {
+    black_box(jotdown::Parser::new(black_box(bench_input::ALL))).last()
+}
+
+fn full() -> String {
+    let mut s = String::new();
+    jotdown::html::Renderer
+        .push(jotdown::Parser::new(bench_input::ALL), &mut s)
+        .unwrap();
+    s
+}
+
+iai::main!(block, block_inline, full);
