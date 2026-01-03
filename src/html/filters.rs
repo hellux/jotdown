6a4528812cd9a4621b56dab02b724f5d5a5371a1
Dawid Ciężarkiewicz <dpc@dpc.pw>
2026-01-02 23:29:14 -0800

    chore: async version of sanitize filter

--- src/html/filters.rs
+++ src/html/filters.rs
@@ -78,6 +78,64 @@ pub trait SanitizeExt {
 
 impl<'s, R> SanitizeExt for R where R: Sized + Render<'s> {}
 
+/// Async version of Sanitize filter for async rendering
+pub struct AsyncSanitize<R>(R);
+
+#[async_trait::async_trait]
+impl<'s, R> crate::r#async::AsyncRender<'s> for AsyncSanitize<R>
+where
+    R: crate::r#async::AsyncRender<'s> + Send,
+{
+    type Error = R::Error;
+
+    async fn emit(&mut self, event: Event<'s>) -> Result<(), Self::Error> {
+        match event {
+            // Render raw html as code blocks to avoid arbitrary html injection.
+            Event::Start(Container::RawBlock { format }, _attrs)
+            | Event::Start(Container::RawInline { format }, _attrs)
+                if format == "html" =>
+            {
+                self.0
+                    .emit(Event::Start(
+                        Container::CodeBlock { language: format },
+                        Attributes::new(),
+                    ))
+                    .await
+            }
+            // Strip all attributes from containers
+            Event::Start(container, _attrs) => {
+                self.0
+                    .emit(Event::Start(container, Attributes::new()))
+                    .await
+            }
+            _ => self.0.emit(event).await,
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl<'s, R> crate::r#async::AsyncRenderOutput<'s> for AsyncSanitize<R>
+where
+    R: crate::r#async::AsyncRenderOutput<'s> + Send,
+{
+    type Output = R::Output;
+
+    fn into_output(self) -> Self::Output {
+        self.0.into_output()
+    }
+}
+
+pub trait AsyncSanitizeExt {
+    fn sanitize(self) -> AsyncSanitize<Self>
+    where
+        Self: Sized,
+    {
+        AsyncSanitize(self)
+    }
+}
+
+impl<'s, R> AsyncSanitizeExt for R where R: Sized + crate::r#async::AsyncRender<'s> {}
+
 #[test]
 fn sanitize_me() {
     use crate::RenderOutputExt;
