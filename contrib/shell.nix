60634e7e7dbca5e5d241cf938b7af7b77522f89f
Noah Hellman <noah@hllmn.net>
2026-05-24 00:02:09 +0200

    contrib: add shell.nix
    dependencies for running tests, lint, fuzz, wasm

---
+++ contrib/shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? (import (builtins.fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/nixos-25.11.tar.gz";
+  sha256 = "sha256:06rhb49ksbc07lp31xgf8qdlph29knrql30x58s4x3fflcw8zkg5";
+}) { }), use_system_vim ? false, ... }:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.bmake
+    pkgs.cacert # npm install
+    pkgs.cargo
+    pkgs.clippy
+    pkgs.git # submodules
+    pkgs.lld # wasm-pack
+    pkgs.nodejs # djot.js
+    pkgs.python3 # jotdown_wasm http server
+    pkgs.rustc
+    pkgs.wasm-pack # jotdown_wasm
+  ];
+
+  MIME_TYPES = "${pkgs.mailcap}/etc/nginx/mime.types";
+}
