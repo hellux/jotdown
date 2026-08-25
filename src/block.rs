64a4bf89f729b78d7f2cc230341285207590e954
Noah Hellman <noah@hllmn.net>
2026-08-25 18:58:09 +0200

    Revert "block: fix div closed by indented code block"
    This reverts commit 41f3f045a6083ccda9ced5a1d5170e27eeff8b46.

--- src/block.rs
+++ src/block.rs
@@ -1426,14 +1426,12 @@ impl<'s> Kind<'s> {
                 kind,
                 has_closing_fence,
                 nested_raw,
-                indent,
                 ..
             } => {
                 if let Kind::Fenced {
                     kind: k,
                     fence_length: l,
                     spec,
-                    indent: i,
                     ..
                 } = IdentifiedBlock::new(line).kind
                 {
@@ -1443,7 +1441,7 @@ impl<'s> Kind<'s> {
                         }
                     } else if k == *kind {
                         *has_closing_fence = l >= *fence_length && spec.is_empty();
-                    } else if *kind == FenceKind::Div && i <= *indent {
+                    } else if *kind == FenceKind::Div {
                         if let FenceKind::CodeBlock(c) = k {
                             *nested_raw = Some((c, l));
                         }
